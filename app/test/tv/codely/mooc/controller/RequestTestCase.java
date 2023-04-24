8811edf51cd001e0d067a5fc95049c5fe02ed376
FRC <pako.rc10.4@gmail.com>
2023-04-24 21:32:54 +0200

    adaptado a Java 11 y a gradle 7.2

--- app/test/tv/codely/mooc/controller/RequestTestCase.java
+++ app/test/tv/codely/mooc/controller/RequestTestCase.java
@@ -1,17 +1,15 @@
 package tv.codely.mooc.controller;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public abstract class RequestTestCase {
---
+++ app/test/tv/codely/mooc/controller/courses/CoursesPutControllerShould.java
@@ -0,0 +1,12 @@
+package tv.codely.mooc.controller.courses;
+
+import org.junit.jupiter.api.Test;
+import tv.codely.mooc.controller.RequestTestCase;
+
+public class CoursesPutControllerShould extends RequestTestCase {
+
+    @Test
+    void create_a_valid_course(){
+        assertRequestWithBody()
+    }
+}
